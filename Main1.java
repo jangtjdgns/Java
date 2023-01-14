bf7625409cabc2a6752b0f6fb4b24da055b15d19
jangtjdgns <jangtjdgns0208@gmail.com>
2023-01-14 15:42:27 +0900

    1. 출력
    Java
    1.출력 (print)
    
    23.01.12(목)

---
+++ Main1.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    //ln 유무는 줄바꿈
+    System.out.println("안녕하세요!");
+    
+    System.out.print("안녕하세요!.");
+    System.out.print("안녕하세요!.");
+  }
+}
