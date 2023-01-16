39b4785061b7417343e2b9f92ddc8ea01a62b3c6
jangtjdgns <jangtjdgns0208@gmail.com>
2023-01-16 21:58:36 +0900

    _3. 반복문 사용안하는 문제
    Java
    -반복문 사용안하고 반복하기
    
    23.01.16(월)

---
+++ _반복문_사용_전2.java
@@ -0,0 +1,24 @@
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : dan 변수의 값에 따라 첫번째로 곱하는 수가 변하게 해주세요.
+
+class Exam02 {
+	public static void main(String[] args) {
+		int dan = 8;
+
+		System.out.println(dan + " * 1 = 8");
+		System.out.println(dan + " * 2 = 16");
+		System.out.println(dan + " * 3 = 24");
+		System.out.println(dan + " * 4 = 32");
+		System.out.println(dan + " * 5 = 40");
+		System.out.println(dan + " * 6 = 48");
+		System.out.println(dan + " * 7 = 56");
+		System.out.println(dan + " * 8 = 64");
+		System.out.println(dan + " * 9 = 72");
+
+		// 출력
+		/*
+		 * 8 * 1 = 8 8 * 2 = 16 8 * 3 = 24 8 * 4 = 32 8 * 5 = 40 8 * 6 = 48 8 * 7 = 56 8
+		 * * 8 = 64 8 * 9 = 72
+		 */
+	}
+}
